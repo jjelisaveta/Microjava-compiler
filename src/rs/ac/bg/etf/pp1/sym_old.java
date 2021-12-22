bb533c95891c19d9056fd2fd0038c241a7e6c576
Rica <jelisavetajevtic@gmail.com>
2021-12-22 12:57:51 +0100

    first commit

---
+++ src/rs/ac/bg/etf/pp1/sym_old.java
@@ -0,0 +1,65 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym_old {
+	//keywords
+	public static final int PROG = 1;
+	public static final int BREAK = 2;
+	public static final int PRINT = 3;
+	public static final int RETURN = 4;
+	public static final int VOID = 5;
+	public static final int CLASS = 6;
+	public static final int ENUM = 7;
+	public static final int IF = 8;
+	public static final int ELSE = 9;
+	public static final int CONST = 10;
+	public static final int DO = 11;
+	public static final int WHILE = 12;
+	public static final int NEW = 13;
+	public static final int READ = 14;
+	public static final int EXTENDS = 15;
+	public static final int CONTINUE = 16;
+	public static final int THIS = 17;
+	public static final int SUPER = 18;
+	public static final int GOTO = 19;
+	public static final int RECORD = 20;
+	
+	//identifiers
+	public static final int IDENT = 21;
+	
+	//constants
+	public static final int NUMBER = 22;
+	public static final int BOOL  = 23;
+	public static final int CHAR = 24;
+	
+	//operators
+	public static final int PLUS = 25;
+	public static final int MINUS = 26;
+	public static final int MUL = 27;
+	public static final int DIV = 28;
+	public static final int MOD = 29;
+	public static final int EQ = 30;
+	public static final int NE = 31;
+	public static final int GT = 32;
+	public static final int GE = 33;
+	public static final int LT = 34;
+	public static final int LE = 35;
+	public static final int AND = 36;
+	public static final int OR = 37;
+	public static final int EQUAL = 38;
+	public static final int INC = 39;
+	public static final int DEC = 40;
+	public static final int SEMI = 41;
+	public static final int COLON = 42;
+	public static final int COMMA = 43;
+	public static final int POINT = 44;
+	public static final int LPAREN = 45;
+	public static final int RPAREN = 46;
+	public static final int LBRACKET = 47;
+	public static final int RBRACKET = 48;
+	public static final int LBRACE = 49;
+	public static final int RBRACE = 50;
+	
+	public static final int EOF = 51;
+}
+
+
